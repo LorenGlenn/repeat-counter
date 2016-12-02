d61e2b1a1705849efba7e78efb172a263f08f329
Loren Glenn <lorencglenn@gmail.com>
2016-12-02 08:36:46 -0800

    add constructor for repeatcounter class with getters and setters

--- Objects/RepeatCounter.cs
+++ Objects/RepeatCounter.cs
@@ -4,6 +4,33 @@ namespace WordCounter.Objects
 {
   public class RepeatCounter
   {
+    private string _userSentence;
+    private string _userWordToSearch;
 
+    public RepeatCounter(string inputSentence, string inputWordToSearch)
+    {
+      _userSentence = inputSentence;
+      _userWordToSearch = inputWordToSearch;
+    }
+
+    public string GetUserSentence()
+    {
+      return _userSentence;
+    }
+
+    public string GetUserWordToSearch()
+    {
+      return _userWordToSearch;
+    }
+
+    public void SetUserSentence(string newSentence)
+    {
+      _userSentence = newSentence;
+    }
+
+    public void SetUserWordToSearch(string newWordToSearch)
+    {
+      _userWordToSearch = newWordToSearch;
+    }
   }
 }
