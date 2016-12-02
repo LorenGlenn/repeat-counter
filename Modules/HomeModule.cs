33673ac7b7971adfb1c880a266026e380d1eb9ea
Loren Glenn <lorencglenn@gmail.com>
2016-12-02 09:10:35 -0800

    add html and routing to complete initial front end

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -10,7 +10,14 @@ namespace WordCounter
     public HomeModule()
     {
       Get["/"]=_=> View["index.cshtml"];
-      
+      Post["/count"]=_=>
+      {
+        string inputSentence = Request.Form["sentence"];
+        string inputWordToSearch = Request.Form["word"];
+        RepeatCounter newCounter = new RepeatCounter(inputSentence, inputWordToSearch);
+        int result = newCounter.CountRepeats();
+        return View["result.cshtml", result];
+      }
     }
   }
 }
